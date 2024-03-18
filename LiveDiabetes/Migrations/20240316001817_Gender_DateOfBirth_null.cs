51b5fb76b75b4cdbf17c5f977ca473fdf3755edf
jdhatad23 <joao.ramos.aesa@gmail.com>
2024-03-18 16:20:11 +0000

    FirstCommit

---
+++ LiveDiabetes/Migrations/20240316001817_Gender_DateOfBirth_null.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LiveDiabetes.Migrations
+{
+    /// <inheritdoc />
+    public partial class Gender_DateOfBirth_null : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "nvarchar(10)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "nvarchar(10)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+        }
+    }
+}
---
+++ LiveDiabetes/Migrations/20240316003458_TudoNull2.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LiveDiabetes.Migrations
+{
+    /// <inheritdoc />
+    public partial class TudoNull2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(20)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldNullable: true);
+        }
+    }
+}
